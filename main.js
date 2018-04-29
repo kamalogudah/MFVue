0f73c834c8a7b884811c5317ad5f51e9846f6ad7
kamalogudah <mcpaul2058@gmail.com>
2018-04-29 23:38:25 +0300

    The watch property

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ new Vue({
   data: {
     counter: 0,
     x: 0,
-    y: 0
+    y: 0,
+    result: ''
   },
   methods: {
     increase: function(step) {
       this.counter += step;
+      this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5'
     },
     updateCoordinates (event) {
       this.x = event.clientX
@@ -17,6 +19,14 @@ new Vue({
     },
     alertMe() {
       alert('Alert!');      
+    }
+  },
+  watch: {
+    counter: function(value) {
+      var vm = this;
+      setTimeout(function(){
+        vm.counter = 0;
+      }, 2000);
     }
   }
 })
