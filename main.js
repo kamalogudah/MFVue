d86eac5157b72af0a7bfec259a4ef461a6e44b43
kamalogudah <mcpaul2058@gmail.com>
2018-04-29 22:47:40 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+new Vue({
+  el: '#app',
+  data: {
+    counter: 0,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    increase: function(step) {
+      this.counter += step;
+    },
+    updateCoordinates (event) {
+      this.x = event.clientX
+      this.y = event.clientY
+
+    },
+    alertMe() {
+      alert('Alert!');      
+    }
+  }
+})
